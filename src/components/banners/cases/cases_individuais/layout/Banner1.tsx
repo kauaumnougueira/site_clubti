22d008b855f65469eff3b610c44cc6aaf8706eb0
Kaua Nogueira <kaua_kna@hotmail.com>
2024-10-02 10:48:15 -0300

    deploy 4 teste

--- src/components/banners/cases/cases_individuais/layout/Banner1.tsx
+++ src/components/banners/cases/cases_individuais/layout/Banner1.tsx
@@ -1,3 +1,4 @@
+import * as S from "../../../../../assets/styleds/StyledComponents";
 import Banner1Layout from "../../../../layout/Banner1Layout";
 import {Banner1Props} from "../interfaces"
 
@@ -5,7 +6,7 @@ const Banner1 = ({ chamada }: Banner1Props) => {
     return (
         <Banner1Layout height="80vh" mobileHeight="80vh" img={<img src={chamada.banner} alt="Banner" width="553" height="497" className="max-w-full h-auto" />}>
             <img src={chamada.logo} width="393" height="102" className="mb-5" />
-            <p className="font-bold text-4xl"> {chamada.texto}</p>
+            <S.Primario2Text className="font-bold text-4xl"> {chamada.texto}</S.Primario2Text>
         </Banner1Layout>
     );
 };
